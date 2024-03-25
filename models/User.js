7514efa6746b4136c9a1bb69277da6153a92bca2
Lesego <lesego@mydough.co.za>
2024-03-25 23:53:54 +0200

    Added property and user database models

---
+++ models/User.js
@@ -0,0 +1,26 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: [true, 'Email already exists']
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: [true, 'Email already exists']
+    },
+    image: {
+        type: String
+    },
+    bookmarks: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Property'
+        }
+    ]
+}, {timestamps: true});
+
+const User = models.user || model('User', UserSchema);
+
+export default User;
