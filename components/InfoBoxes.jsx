bccc33aa7b552615bf4b68caa7610c0460b14fc3
Moabi7 <ramatsuilm@outlook.com>
2024-03-26 22:22:28 +0200

    Minor fix word fix

--- components/InfoBoxes.jsx
+++ components/InfoBoxes.jsx
@@ -17,7 +17,7 @@ const InfoBoxes = () => {
         Find your dream rental property. Bookmark properties and contact owners.
         </InfoBox>
         <InfoBox
-            heading='For Renters'
+            heading='For Property Owners'
             backgroundColor='bg-blue-100'
             buttonInfo={{
                 text: 'Add Property',
