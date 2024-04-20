2d5c2bdeb02ac46d0c9c40abe023e09494c4dfe7
Moabi7 <ramatsuilm@outlook.com>
2024-04-20 21:06:28 +0200

    added next auth route

---
+++ utils/authOptions.js
@@ -0,0 +1,34 @@
+import GoogleProvider from 'next-auth/providers/google';
+
+export const authOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            authorization: {
+                params: {
+                  prompt: "consent",
+                  access_type: "offline",
+                  response_type: "code"
+                }
+            }
+        })
+    ],
+    callbacks: {
+        // Invoked on successfu signin
+        async signIn ({ profile }) {
+            // 1. Connect to database
+            // 2. Check if usr exists
+            // 3. If not, create new user
+            // 4. Return true to allow sign in
+
+        },
+
+        // Modifies the session object
+        async session ({ session }) {
+            // 1. Get user from database
+            // 2. Add user to session
+            // 3. return session
+        }
+    }
+};
