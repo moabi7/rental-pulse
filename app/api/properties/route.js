4a25ebb8e4ccfb05461d706f4109008ff82d56ed
Moabi7 <ramatsuilm@outlook.com>
2024-04-21 19:58:31 +0200

    Cloudinary image upload

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,8 +1,9 @@
 import connectDb from "@/config/database";
 import Property from "@/models/Property";
 import { getSessionUser } from "@/utils/getSessionUser";
+import cloudinary from "@/config/cloudinary";
 
-// GET /api/properties
+// GET /api/properties 
 export const GET = async (request) => {
     try {
         await connectDb();
@@ -59,8 +60,31 @@ export const POST = async (request) => {
                 phone: formData.get("seller_info.phone")
             },
             owner: userId,
-            // images
         };
+
+        // upload images to cloudinary
+        const imageUploadPromises = [];
+
+        for (let image of images) {
+            const imageBuffer = await image.arrayBuffer();
+            const imageArray = Array.from(new Uint8Array(imageBuffer));
+            const imageData = Buffer.from(imageArray);
+
+            // Convert the image data to base64
+            const imageBase64 = imageData.toString('base64');
+
+            // Make request to upload to Cloudinary
+            const result = await cloudinary.uploader.upload(`data:image/png;base64,${imageBase64}`, {
+                folder: 'propertypulse'
+            });
+
+            imageUploadPromises.push(result.secure_url);
+
+            // Wait for all images to upload
+            const uploadedImages = await Promise.all(imageUploadPromises);
+            // Add uploaded images to the propertyData object
+            propertyData.images = uploadedImages;
+        }
 
         const newProperty = new Property(propertyData);
         await newProperty.save();
