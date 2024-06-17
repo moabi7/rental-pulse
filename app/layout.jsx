a9427014ff119f46368d9f6e489245f348627b90
Lesego <lesego@mydough.co.za>
2024-06-17 20:03:25 +0200

    Added react photo swipe gallery

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,11 @@
-import '@/assets/styles/globals.css';
 import NavBar from '@/components/NavBar';
 import Footer from '@/components/Footer';
 import AuthProvider from '@/components/AuthProvider';
 import { ToastContainer } from 'react-toastify';
-import'react-toastify/dist/ReactToastify.css';
 import { GlobalProvider } from '@/context/GlobalContext';
+import '@/assets/styles/globals.css';
+import'react-toastify/dist/ReactToastify.css';
+import 'photoswipe/dist/photoswipe.css'
 
 export const metadata = {
     title: 'PropertyPulse | Find The Perfect Rental',
