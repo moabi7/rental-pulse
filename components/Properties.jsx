6252bcbb41ff984f799df9fb63a5995f9e720d38
Lesego <lesego@mydough.co.za>
2024-06-17 19:42:10 +0200

    Added pagination comoponent

--- components/Properties.jsx
+++ components/Properties.jsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from 'react';
 import PropertyCard from '@/components/PropertyCard';
 import Spinner from '@/components/Spinner';
+import Pagination from '@/components/Pagination';
 
 const Properties = async () => {
     const [loading , setLoading] = useState(true);
@@ -29,21 +30,31 @@ const Properties = async () => {
         };
 
         fetchProperties();
-    }, []);
+    }, [page, pageSize]);
+
+
+    const handlePageChange = (newPage) => {
+        setPage(newPage);
+    };
 
   return loading ? (<Spinner /> ) : (
     <section className="px-4 py-6">
-    <div className="container-xl lg:container m-auto">
-    <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">All Properties</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
-        {
-          properties.length === 0 ? 
-          (<p>No properties found</p>) :
-          properties.map((property) => <PropertyCard key={property._id} property={property}/>)
-        }
-      </div>
-    </div>
-  </section>
+        <div className="container-xl lg:container m-auto">
+            <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">All Properties</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
+                {
+                properties.length === 0 ? 
+                (<p>No properties found</p>) :
+                properties.map((property) => <PropertyCard key={property._id} property={property}/>)
+                }
+            </div>
+            <Pagination page={page}
+                pageSize={pageSize}
+                totalItems={totalItems}
+                onPageChange={handlePageChange}
+            />
+        </div>
+    </section>
   )
 };
 
