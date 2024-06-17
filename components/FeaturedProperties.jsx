e9c54c09c7a5081b76b163a28837745bf5b98f4b
Lesego <lesego@mydough.co.za>
2024-06-17 21:47:47 +0200

    Added Featured Property card

--- components/FeaturedProperties.jsx
+++ components/FeaturedProperties.jsx
@@ -1,22 +1,22 @@
-import PropertyCard from '@/components/PropertyCard';
+import FeaturePropertyCard from '@/components/FeaturePropertyCard';
 import { fetchProperties } from '@/utils/requests';
 
 const FeaturedProperties = async () => {
     const data = await fetchProperties({ showFeatured: true });
     const properties = data.properties;
-  return (
+  return properties.length > 0 && (
     <section className="bg-blue-50 px-4 pt-6 pb-10">
         <div className="container-xl lg:container m-auto">
-          <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
-            Featured Properties
-          </h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
+                Featured Properties
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 {
                     properties.length === 0 ? 
                     (<p>No properties found</p>) :
-                    properties.map((property) => <PropertyCard key={property._id} property={property}/>)
+                    properties.map((property) => <FeaturePropertyCard key={property._id} property={property}/>)
                 }
-          </div>
+            </div>
         </div>
     </section>
   )
